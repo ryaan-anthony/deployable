68df091e42b2e9c8ffd4095ab9ac8004a7f49ad6
Tulino, Ryan <rtulino@ebay.com>
2015-09-19 23:24:27 -0400

    Add style

--- views/head.html.php
+++ views/head.html.php
@@ -13,4 +13,6 @@
     div.container {width:900px; margin: 20px auto;}
     p {padding: 5px; min-height: 200px;}
     p.error {color: white; background: grey;}
+    ul,li{list-style: none;}
 </style>
+<title>...</title>
